592fd850f5da770f432bde6e8ce0866ba13b8459
John Panos <johnnypanos@icloud.com>
2018-03-31 14:39:29 -0600

    Fixed vision code

--- constants.py
+++ constants.py
@@ -1,14 +1,26 @@
-CAM_LOCATION = 0 # will eventually be set to MJPG stream address: http://roborio-3256-frc.local:1181/?action=stream. set to 0 for camera directly connected to TX1 over USB
+# Network Tables
 ROBORIO_IP = '10.32.56.2' # needed for NetworkTables
-NEURAL_NETWORK = False
-
-MODEL_FILENAME = 'model.caffemodel' # filename of the DetectNet model file
-PROTO_FILENAME = 'proto.prototxt' # filename of the DetectNet prototxt
-
-SHOW_FRAMES = False # whether or not to display frames using opencv windows
 SEND_COORDS = False # whether or not to send coordinates of bboxes over NetworkTables
 
+# Camera
+CAM_LOCATION = 0
 WIDTH_RES = 360
 FOV = 1.0
 FOCAL = WIDTH_RES / FOV
 RATIO_SCALE = (1280.0/720.0) / (360.0/240.0) # needed because processed images are resized to 360x240
+SHOULD_RESIZE = True
+RESIZE_RES = (240, 160)
+
+# Neural Network
+NEURAL_NETWORK = False
+MODEL_FILENAME = 'model.caffemodel'
+PROTO_FILENAME = 'proto.prototxt'
+
+# OpenCV
+SHOW_FRAMES = False
+
+# Stream
+STREAM_IP = '10.32.56.179'
+STREAM_PORT = 5805
+
+
